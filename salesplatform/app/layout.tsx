0345dc895fa0a7fc1f294021a4ad958e0124dcbc
oscar-barreto <88983667+oscar-barreto@users.noreply.github.com>
2023-10-16 16:52:08 +0200

    first commit

---
+++ salesplatform/app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const poppins = Poppins({ subsets: ['latin'],weight:['400', '700'] })
+
+export const metadata: Metadata = {
+  title: 'ESHOP',
+  description: 'Plataform for online sales',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <div className='flex flex-col min-h-screen'>
+        <Navbar/>
+        <main className='flex grow'>{children}</main>
+        <Footer/>
+        </div>
+        </body>
+    </html>
+  )
+}
