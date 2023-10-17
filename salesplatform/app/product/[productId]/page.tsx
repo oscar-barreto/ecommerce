7b7ce16b3417b8647409d1466d04f6fff3063487
oscar-barreto <88983667+oscar-barreto@users.noreply.github.com>
2023-10-17 16:06:13 +0200

    Product Page almost done

--- salesplatform/app/product/[productId]/page.tsx
+++ salesplatform/app/product/[productId]/page.tsx
@@ -1,10 +1,16 @@
+import { product } from "@/app/utils/product"
+import Container from "@/app/components/Container"
+import ProductDetails from "./ProductDetails"
+
 interface IPrams{
     productId?:string
 }
 const ProductPage = ({params}:{params:IPrams}) => {
   return (
-    <div>
-        
+    <div className="p-8">
+        <Container>
+            <ProductDetails product={product}/>
+        </Container>
     </div>
   )
 }
