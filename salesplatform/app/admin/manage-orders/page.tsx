f5ebc197db42be6c2b557fd05a70346c3c9ecf73
oscar-barreto <88983667+oscar-barreto@users.noreply.github.com>
2023-11-28 16:44:43 +0100

    updates

--- salesplatform/app/admin/manage-orders/page.tsx
+++ salesplatform/app/admin/manage-orders/page.tsx
@@ -1,8 +1,24 @@
+import Container from "@/app/components/Container";
+import ManageOrdersClient from "./ManageOrdersClient";
+import getOrders from "@/actions/getOrders";
+import getCurrentUser from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
 
-const ManageOrders = () => {
+const ManageOrders = async () => {
+  const orders = await getOrders();
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops! Access denied." />;
+  }
+
   return (
-    <div>ManageOrders</div>
-  )
-}
+    <div className="pt-8">
+      <Container>
+        <ManageOrdersClient orders={orders} />
+      </Container>
+    </div>
+  );
+};
 
-export default ManageOrders
+export default ManageOrders;
